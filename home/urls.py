d1391514580f2db9cc7f03ce0fabc94acb205666
Wojciech Warych <woj.warych@gmail.com>
2017-08-22 16:33:54 +0200

    removed redundant urls

--- home/urls.py
+++ home/urls.py
@@ -1,11 +1,14 @@
 from django.conf.urls import url
 from . import views
 
+
+
 app_name = 'home'
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
-	url(r'^example/', views.example_page, name='example_page'),
-	url(r'^about/', views.about, name='about'),
-	url(r'^contact/', views.contact, name='contact'),
-	url(r'^quizes/', views.quizes, name='quizes')
+	url(r'^about/$', views.about, name='about'),
+	url(r'^contact/$', views.contact, name='contact'),
+	url(r'^quizes/$', views.quizes, name='quizes'),
+	url(r'^posts/(?P<post_id>\d+)/$', views.post, name='post'),
+	url(r'^msg/$', views.send_msg, name='send_msg')
 ]
