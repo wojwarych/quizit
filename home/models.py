1130b1545835d1e37887ebe3a385ef6dea26e617
Wojciech Warych <woj.warych@gmail.com>
2017-08-22 16:33:03 +0200

    model for news post

--- home/models.py
+++ home/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+
+class News(models.Model):
+
+
+	title = models.CharField(max_length=200)
+	article = models.TextField()
+	pub_date = models.DateTimeField()
+
+
+	def __str__(self):
+
+
+		return self.title
