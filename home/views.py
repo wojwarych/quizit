2a8d419f88bd8d6e90089b8691224cc1827eab0d
Wojciech Warych <woj.warych@gmail.com>
2017-08-22 16:36:07 +0200

    added contact form

--- home/views.py
+++ home/views.py
@@ -1,11 +1,20 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect, reverse
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import News
+from .forms import ContactForm
+
+from django.core.mail import EmailMessage
+from django.template import Context
+from django.template.loader import get_template
+
 
 
 def index(request):
 
+	query = News.objects.order_by('-pub_date')[:5]
 
-	return render(request, 'home/index.html')
+	context = {'news': query}
+	return render(request, 'home/index.html', context)
 
 
 def example_page(request):
@@ -23,10 +32,50 @@ def about(request):
 def contact(request):
 
 
-	return render(request, 'home/contact.html')
+	contact_form = ContactForm
+
+	if request.method == 'POST':
+		form = contact_form(data=request.POST)
+
+		if form.is_valid():
+			contact_name = request.POST.get('name', '')
+			contact_email = request.POST.get('email', '')
+			content = request.POST.get('message', '')
+
+			template = get_template('contact_template.txt')
+			context = {
+				'contact_name': contact_name,
+				'contact_email': contact_email,
+				'form_content': content,
+			}
+			content = template.render(context)
+
+			email = EmailMessage(
+				"New contact form submission",
+				content,
+				"Your website" +'',
+				['woj.warych@gmail.com'],
+				headers = {'Reply-To': contact_email }
+			)
+			email.send()
+			return redirect('home:contact')
+
+
+	return render(request, 'home/contact.html', {'form': contact_form})
 
 
 def quizes(request):
 
 
 	return render(request, 'home/quiz_nav.html')
+
+
+def post(request, post_id):
+
+
+	the_post = News.objects.get(id=post_id)
+	context = {'post': the_post}
+
+
+	return render(request, 'home/post.html', context)
+
