d7bb9200ca5f5b4fe6efc33208296ed8b6a35a58
Wojciech Warych <woj.warych@gmail.com>
2017-08-28 19:13:47 +0200

    added article snippet row

--- home/models.py
+++ home/models.py
@@ -7,6 +7,7 @@ class News(models.Model):
 
 	title = models.CharField(max_length=200)
 	article = models.TextField()
+	art_snippet = models.CharField(default='...', max_length=500)
 	pub_date = models.DateTimeField()
 
 
